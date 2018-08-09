d313388e2d6393c96eb6d912da6c4ea12f4d3b2d
com <com@10.105.54.105>
2018-08-09 17:50:50 +0900

    Start Adding GUI

---
+++ CraftCreate/src/net/xpoint/craftcreate/gui/CCFrame.java
@@ -0,0 +1,17 @@
+package net.xpoint.craftcreate.gui;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class CCFrame extends JFrame{
+
+	public CCFrame() {
+		setTitle("Craft Create! [Dev Version]");
+		setSize(new Dimension(800, 600));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setVisible(true);
+	}
+	
+}
